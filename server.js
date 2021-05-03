2273953e9ccac4d09329b88e48ca829abe6737b9
ahmad afridi <ahmadafridi194@gmail.com>
2021-05-03 12:22:38 -0600

    sorting dates desc

--- server.js
+++ server.js
@@ -227,7 +227,7 @@ router.post('/getWeekWiseData', function(req, res) {
         }]
 
         let finalData = []
-        finalData.sort()
+        finalData.sort().reverse();
         if(user['totals'] && Array.isArray(user['totals']) && user['totals'].length > 0){
             totals.forEach(x=>{
                 user['totals'].forEach(y=>{
